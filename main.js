d8b10c0d09302652cd8d6c95303433ede5c67eb5
Rufat-J <rj.7@live.se>
2022-11-23 01:31:23 +0100

    homepage buttons and sound effect

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+
 let userScore = 0;
 let computerScore = 0;
 
@@ -105,3 +106,4 @@ function main() {
 }
 
 main();
+
